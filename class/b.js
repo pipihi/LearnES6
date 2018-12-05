467acf2cd27673eb0a56c11d841b63d94df5d74c
liugh2 <liugh2@adnonstop.com>
2018-12-05 10:25:07 +0800

    fix

--- class/b.js
+++ class/b.js
@@ -1,15 +1,18 @@
+// 依赖导入
 import {ClassA} from './a.js'
 
+// 继承
 class ClassB extends ClassA{
 
     constructor() {
+        // 调用父类的构造函数
         super("Super Man")
     }
-
+    
+    // 方法重写
     sayHi() {
         alert(this.name + ":Hello!");
     }
 }
-
 
 export  {ClassB};
