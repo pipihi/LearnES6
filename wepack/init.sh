53db7ddc6969eac3eae980b0c91c1814e1b6a463
liugh2 <liugh2@adnonstop.com>
2018-12-11 10:30:36 +0800

    fix

--- wepack/init.sh
+++ wepack/init.sh
@@ -1,5 +1,7 @@
-# 在这之前首先要安装node
-#  npm init
+# 1.在这之前首先要安装node
+# 2.进入index.html目录执行
+npm install node-http-server --save-dev
+
 npm install webpack --save-dev
 npm install babel-core --save-dev
 npm install babel --save-dev
